e6cf132e6f517f18c4636580810f5bd77452a6d4
LobsterCheese <68477510+LobsterCheese@users.noreply.github.com>
2026-03-08 11:48:11 -0700

    pitcher minigame
    added pitcher minigame prefab

---
+++ Assets/MinigameScriptsAnderson/shakingPitcher.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
+
+public class shakingPitcher : MonoBehaviour
+{
+
+    public float wigglePower = 4f;
+    public float followSpeed = 4f;
+
+    private Vector2 mousePos;
+
+    private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        rb.MovePosition(mousePos);
+        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.transform.rotation *= Quaternion.Euler(0, 0, wigglePower * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.transform.rotation *= Quaternion.Euler(0, 0, -wigglePower * Time.deltaTime);
+        }
+        else
+        {
+            rb.transform.rotation *= Quaternion.Euler(0, 0, 0);
+        }
+
+    }
+}
