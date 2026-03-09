f271c8c9165fb265781a855e21f7eea1d19eaaeb
LobsterCheese <68477510+LobsterCheese@users.noreply.github.com>
2026-03-08 20:48:09 -0700

    working on shaun's evil minigame

--- Assets/MinigameScriptsAnderson/TableWiping.cs
+++ Assets/MinigameScriptsAnderson/TableWiping.cs
@@ -3,8 +3,17 @@ using UnityEngine;
 
 public class TableWiping : MonoBehaviour
 {
-    [SerializeField] float wipeAmount = 300;
+    [SerializeField] float wipeAmount = 255;
     [SerializeField] minigameExit parent;
+
+    public GameObject stain1;
+    public GameObject stain2;
+    public GameObject stain3;
+
+    public float timer = 2f;
+
+    public GameObject winCard;
+
     float dist = 0;
     Vector3 prevPos;
 
@@ -19,9 +28,31 @@ public class TableWiping : MonoBehaviour
         dist += (transform.position - prevPos).magnitude;
         prevPos = transform.position;
 
+        stain1.GetComponent<SpriteRenderer>().material.color = new Color(1, 1, 1, wipeAmount/255);
+        stain2.GetComponent<SpriteRenderer>().material.color = new Color(1, 1, 1, wipeAmount/255);
+        stain3.GetComponent<SpriteRenderer>().material.color = new Color(1, 1, 1, wipeAmount/255);
+
+        if (winCard.activeInHierarchy)
+        {
+            timer -= 1f * Time.deltaTime;
+        }
+
+        if (timer < 0)
+        {
+            if (!parent.exit)
+            {
+                parent.exit = true;
+            }
+        }
+
+
         if (dist >= wipeAmount)
         {
-            parent.exit = true;
+            //parent.exit = true;
+            if (!winCard.activeInHierarchy)
+            {
+                winCard.SetActive(true);
+            }
         }
     }
 }
