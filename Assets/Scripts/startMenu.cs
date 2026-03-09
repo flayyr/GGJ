3a1fdaaabe370f99b6a60e25bb552f7f2382a21d
grappl47 <fx2125@nyu.edu>
2026-03-09 15:53:52 -0700

    bobbing button

--- Assets/Scripts/startMenu.cs
+++ Assets/Scripts/startMenu.cs
@@ -3,36 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class startMenu : MonoBehaviour
 {
-    private int moving;
-    private int moveSpeed = 4;
-    private float offset = 40;
-    private float yOffsetUp;
-    private float yOffsetDown;
+    [SerializeField] float bobAmount;
+    [SerializeField] float bobFrequency;
+    float startY;
+    float timer = 0;
 
     public string sceneToGoTo;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        moving = Mathf.RoundToInt(transform.position.y);
-        yOffsetUp = transform.position.y + offset;
-        yOffsetDown = transform.position.y - offset;
+        startY = transform.position.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log(moving);
-
-        if(moving >= yOffsetUp)
-        {
-            moving -= moveSpeed;
-        }
-        else if(moving <= yOffsetDown)
-        {
-            moving += moveSpeed;
-        }
-            Vector2.Lerp(new Vector2(transform.position.x, yOffsetUp), new Vector2(transform.position.x, yOffsetDown), moving * Time.deltaTime);
+        timer += Time.deltaTime;
+        transform.position =new Vector3(transform.position.x, startY + bobAmount * Mathf.Sin(bobFrequency* timer), transform.position.z);
     }
 
     public void SwitchScene()
